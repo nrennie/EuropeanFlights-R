0dd071de526116f13b1ab287972c2d6f044583ef
nrennie <nrennie35@gmail.com>
2022-12-29 01:00:49 +0000

    use ggplot instead of plotly

--- app.R
+++ app.R
@@ -1,9 +1,7 @@
 library(dplyr)
 library(shiny)
 library(ggplot2)
-library(stringr)
 library(markdown)
-library(plotly)
 
 flights <- readr::read_csv("flights_data.csv")
 
@@ -58,15 +56,13 @@ ui <- fluidPage(
                The number of flights arriving or leaving from European airports saw a dramatic decrease with the onset of the Covid-19 pandemic in March 2020. Amsterdam - Schipol remains the busiest airport, averaging 1,150 flights per day since January 2016.
                
                Data: [Eurocontrol](https://ansperformance.eu/data/)
-               
-               ### Total number of flights per week
                "),
       
       fluidRow(
         column(12, 
-               # area plot
+               # bar plot
                div(style='height:400px;',
-                   plotlyOutput(outputId = "areaplot", height = "400px")
+                   plotOutput(outputId = "barplot", height = "400px")
                ))
       ),
       
@@ -78,42 +74,29 @@ ui <- fluidPage(
 # Function to define server
 server <- function(input, output) {
   
-  # Filer the data and add tooltip
-  area_df <- reactive({
+  # Filer the data
+  plot_df <- reactive({
     req(input$country)
     plot_df <- flights %>% 
-      filter(Country %in% input$country) %>% 
-      mutate(`Flights` = paste0(Total, 
-                                "\nWeek Commencing : ", 
-                                Date,
-                                "\nCountry : ", 
-                                Country))
+      filter(Country %in% input$country) 
     plot_df
   })
   
-  # Make the area chart
-  output$areaplot <- renderPlotly({
-    area_data <- area_df()
-    g2 <- ggplot(data = plot_df, 
+  # Make the bar chart
+  output$barplot <- renderPlot({
+    plot_df <- plot_df()
+    ggplot(data = plot_df, 
                  mapping = aes(x = Date,
                                y = Total,
-                               fill = Country,
-                               label = Flights)) +
-      geom_col(alpha = 0.7) +
+                               fill = Country)) +
+      geom_col() +
       labs(x = "",
            y = "Total number of flights per week", 
-           title = "") +
-      scale_fill_manual(values = c("Belgium" = "#8175aa", 
-                                   "France" = "#6fb899", 
-                                   "Ireland" = "#31a1b3", 
-                                   "Luxembourg" = "#ccb22b", 
-                                   "Netherlands" = "#a39fc9", 
-                                   "United Kingdom" = "#94d0c0")) +
+           title = "Total number of flights per week") +
+      scale_fill_brewer(palette = "Dark2") +
       theme_minimal() +
-      theme(plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), unit = "cm"), 
-            panel.grid.major.x = element_blank(),
+      theme(panel.grid.major.x = element_blank(),
             panel.grid.minor.x = element_blank())
-    ggplotly(g2, tooltip = c("label"))
   })
 
 }
