211ebf8c5c92dde852c1467d88927df5519a22e8
nrennie <nrennie35@gmail.com>
2023-01-17 12:04:23 +0000

    restructure server code

--- app.R
+++ app.R
@@ -55,39 +55,35 @@ ui <- fluidPage(
            )
     )
   )
-  
+
+# Function to make the plot
+create_plot <- function(data) {
+  ggplot(data = data, 
+         mapping = aes(x = Date,
+                       y = Total,
+                       fill = Country)) +
+    geom_col() +
+    labs(x = "",
+         y = "Total number of flights per week", 
+         title = "Total number of flights per week") +
+    scale_fill_manual(values = c("Belgium" = "#F2C57C",
+                                 "France" = "#DDAE7E",
+                                 "Ireland" = "#7FB685",
+                                 "Luxembourg" = "#426A5A",
+                                 "Netherlands" = "#EF6F6C",
+                                 "United Kingdom" = "#AC9FBB")) +
+    theme_minimal() +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank())
+}
 
 # Function to define server
 server <- function(input, output) {
   
-  # Filter the data
-  plot_df <- reactive({
-    req(input$country)
-    plot_df <- flights %>% 
-      filter(Country %in% input$country) 
-    plot_df
-  })
-  
-  # Make the bar chart
   output$barplot <- renderPlot({
-    plot_df <- plot_df()
-    ggplot(data = plot_df, 
-                 mapping = aes(x = Date,
-                               y = Total,
-                               fill = Country)) +
-      geom_col() +
-      labs(x = "",
-           y = "Total number of flights per week", 
-           title = "Total number of flights per week") +
-      scale_fill_manual(values = c("Belgium" = "#F2C57C",
-                                   "France" = "#DDAE7E",
-                                   "Ireland" = "#7FB685",
-                                   "Luxembourg" = "#426A5A",
-                                   "Netherlands" = "#EF6F6C",
-                                   "United Kingdom" = "#AC9FBB")) +
-      theme_minimal() +
-      theme(panel.grid.major.x = element_blank(),
-            panel.grid.minor.x = element_blank())
+    req(input$country)
+    new_data <- filter(flights, Country %in% input$country) 
+    create_plot(new_data)
   })
 
 }
